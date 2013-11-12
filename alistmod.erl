0fda5126ebfaf5785c1ae83821a4624aad4bf094
Trevor Barnett <mr.ullet@gmail.com>
2013-11-12 10:59:54 +0000

    :hatching_chick: Another simple list function (and a fix to the module).

--- alistmod.erl
+++ alistmod.erl
@@ -1,8 +1,14 @@
--module(listy).
--export([first/1, last/1]).
+-module(alistmod).
+-export([first/1, last/1, nth/2]).
 
 first([F|_]) -> F.
 
 last([X]) -> X;
 
 last([_|T]) -> last(T).
+
+nth([], _) -> nothing;
+
+nth(L, 0) -> first(L);
+
+nth([_|T], N) -> nth(T, N-1).

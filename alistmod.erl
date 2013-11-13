ad5fd3c8774edb8b1daa3867a317e0cf4bf165c9
Trevor Barnett <mr.ullet@gmail.com>
2013-11-13 17:23:15 +0000

    :hatching_chick: A function to append (join?) two lists.

--- alistmod.erl
+++ alistmod.erl
@@ -1,14 +1,15 @@
 -module(alistmod).
--export([first/1, last/1, nth/2]).
+-compile(export_all).
 
 first([F|_]) -> F.
 
 last([X]) -> X;
-
 last([_|T]) -> last(T).
 
 nth([], _) -> nothing;
-
 nth(L, 0) -> first(L);
-
 nth([_|T], N) -> nth(T, N-1).
+
+append([], L) -> L;
+append([X], L) -> [X|L];
+append([H|T], L) -> [H|append(T, L)].

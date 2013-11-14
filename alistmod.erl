2a1f3054938b58975601a8673f8bc7d974a1ca6f
Trevor Barnett <mr.ullet@gmail.com>
2013-11-14 16:52:56 +0000

    :hatching_chick: Functions to append non-lists to lists and to flatten lists.

--- alistmod.erl
+++ alistmod.erl
@@ -10,6 +10,14 @@ nth([], _) -> nothing;
 nth(L, 0) -> first(L);
 nth([_|T], N) -> nth(T, N-1).
 
-append([], L) -> L;
-append([X], L) -> [X|L];
-append([H|T], L) -> [H|append(T, L)].
+append([], L) when is_list(L) -> L;
+append([X], L) when is_list(L) -> [X|L];
+append([H|T], L) when is_list(L) -> [H|append(T, L)];
+append(X, L) when is_list(L) -> append([X], L);
+append(L, Y) when is_list(L) -> append(L, [Y]);
+append(X, Y) -> [X, Y].
+
+flatten([]) -> [];
+flatten([X]) when is_list(X) -> flatten(X);
+flatten([X]) -> [X];
+flatten([T|H]) -> append(flatten([T]), flatten(H)).

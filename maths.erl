ba5d9ed0cc8b62010afb2fa60104a5315b9ea07f
Trevor Barnett <mr.ullet@gmail.com>
2013-11-15 22:09:20 +0000

    :hatching_chick: Not divisible by zero.

--- maths.erl
+++ maths.erl
@@ -10,5 +10,6 @@ ceiling(X) when (X > 0) -> -trunc(-X-1);
 ceiling(X) -> trunc(X).
 
 divisible(X, X) -> true;
+divisible(X, 0) -> false;
 divisible(X, Y) when abs(Y) > abs(X) -> false; % also covers zero not divisible by anything
 divisible(X, Y) -> X/Y == trunc(X/Y).
